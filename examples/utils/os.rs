13f16aa9eb16de272f6bed830717832ede973364
David Sánchez <davidslt+git@pm.me>
2025-05-16 10:19:29 +0100

    test: fix jwt token private key example (#19)

--- examples/utils/os.rs
+++
@@ -1,8 +0,0 @@
-pub fn env(var: &str) -> Result<String, std::io::Error> {
-    std::env::var(var).map_err(|e| {
-        std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("error loading env var {}: {}", var, e),
-        )
-    })
-}
