2e1481b3a70c9a511177da31d7adc82c994bce2a
David Sánchez <davidslt+git@pm.me>
2025-05-22 12:59:04 +0100

    style: rename error type

--- src/system_identity/generator.rs
+++ src/system_identity/generator.rs
@@ -5,14 +5,14 @@ use crate::{key::creator::Creator as KeyCreator, TokenRetriever};
 use super::iam_client::{IAMClient, SystemIdentityCreationResponseData};
 
 #[derive(Debug, Clone, Error)]
-pub enum SystemIdentityCreationError {
+pub enum SystemIdentityGenerationError {
     // This should probably leverage associated types for the involved traits, so we
     // can gain automatic conversions via #[from] annotations...
     #[error("error retrieving token: `{0}`")]
     TokenRetriever(String),
     #[error("error creating key pair: `{0}`")]
     KeyPairCreator(String),
-    #[error("error creating system identity: `{0}`")]
+    #[error("error retrieving the system identity: `{0}`")]
     IAMClient(String),
 }
 
@@ -40,18 +40,20 @@ where
     T: TokenRetriever,
     I: IAMClient,
 {
-    pub fn generate(self) -> Result<SystemIdentity, SystemIdentityCreationError> {
+    pub fn generate(self) -> Result<SystemIdentity, SystemIdentityGenerationError> {
         let token = self
             .token_retriever
             .retrieve()
-            .map_err(|e| SystemIdentityCreationError::TokenRetriever(e.to_string()))?;
+            .map_err(|e| SystemIdentityGenerationError::TokenRetriever(e.to_string()))?;
         let pub_key = self.key_creator.create().map_err(|_| {
-            SystemIdentityCreationError::KeyPairCreator(String::from("Could not obtain public key"))
+            SystemIdentityGenerationError::KeyPairCreator(String::from(
+                "Could not obtain public key",
+            ))
         })?; // FIXME Creator::Error does not implement std::error::Error, should be convertible to something we can work with
         let SystemIdentityCreationResponseData { client_id, name } = self
             .iam_client
             .create_system_identity(token.access_token(), pub_key.as_slice())
-            .map_err(|e| SystemIdentityCreationError::IAMClient(e.to_string()))?;
+            .map_err(|e| SystemIdentityGenerationError::IAMClient(e.to_string()))?;
 
         Ok(SystemIdentity {
             name,
