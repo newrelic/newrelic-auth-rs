1a2ab867571a6f2ef5fc8fddfc0bb7d8b231bd1c
David Sánchez <davidslt+git@pm.me>
2025-05-28 17:49:36 +0100

    feat(system identity creation): HTTP system identity creator impl

---
+++ examples/generate-l2-si/main.rs
@@ -0,0 +1,126 @@
+//! Full example to generate an L2 System Identity using the `newrelic-auth-rs` library.
+mod client;
+
+use client::HttpClient;
+use dotenvy::dotenv;
+
+use nr_auth::authenticator::HttpAuthenticator;
+use nr_auth::jwt::signer::local::LocalPrivateKeySigner;
+use nr_auth::jwt::signer::JwtSignerImpl;
+use nr_auth::key::creator::KeyType;
+use nr_auth::key::local::{KeyPairGeneratorLocalConfig, LocalCreator};
+
+use nr_auth::key::PrivateKeyPem;
+use nr_auth::system_identity::generator::L2SystemIdentityGenerator;
+use nr_auth::system_identity::iam_client::http_impl::HttpIAMClient;
+use nr_auth::system_identity::input_data::auth_method::{AuthMethod, ClientSecret};
+use nr_auth::system_identity::input_data::environment::NewRelicEnvironment;
+use nr_auth::system_identity::input_data::output_platform::OutputPlatform;
+use nr_auth::system_identity::input_data::{SystemIdentityCreationMetadata, SystemIdentityInput};
+use nr_auth::token_retriever::TokenRetrieverWithCache;
+
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
+
+/// Main function to retrieve and print an access token.
+/// It requires the following environment variables to be set:
+///
+/// PRIVATE_KEY_PATH: Absolute path to the private key associated with the identity
+/// TOKEN_URL: Token verification URL
+/// CLIENT_ID: Identity client_id
+///
+/// # Errors
+/// This function returns an error if:
+/// - The `.env` file is missing or cannot be loaded.
+/// - Required environment variables are not set.
+/// - Any of the components (e.g., signer, client, authenticator) fail to initialize.
+/// - The token retrieval process fails.
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Set the current directory to the example's path
+    let example_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("examples")
+        .join(env!("CARGO_BIN_NAME"));
+    env::set_current_dir(&example_dir).expect("Failed to change directory");
+
+    dotenv().map_err(|e| {
+        format!(".env file not found. Copy .env.dist file to .env and fill the variables: {e}")
+    })?;
+
+    let client_id = env::var("CLIENT_ID")?;
+    let organization_id = env::var("ORGANIZATION_ID")?;
+    // Has a client secret been set?
+    let client_secret_auth_method = env::var("CLIENT_SECRET")
+        .map(ClientSecret::from)
+        .map(AuthMethod::ClientSecret);
+
+    // Has a private key been set to a valid path?
+    let private_key_auth_method = env::var("PRIVATE_KEY_PATH")
+        .map_err(io::Error::other)
+        .map(PathBuf::from)
+        .and_then(|path| fs::read(&path))
+        .map(PrivateKeyPem::from)
+        .map(AuthMethod::PrivateKey);
+
+    // Select one of the two and unwrap. Switch to change priority like this:
+    // let auth_method = private_key_auth_method.or(client_secret_auth_method)?;
+    let auth_method = client_secret_auth_method.or(private_key_auth_method)?;
+
+    let environment = NewRelicEnvironment::Staging;
+
+    let key_path = env::current_dir()?;
+    let output_platform = OutputPlatform::LocalPrivateKeyPath(key_path.to_owned());
+
+    let http_client = HttpClient::new()?;
+    let http_authenticator =
+        HttpAuthenticator::new(http_client.clone(), environment.token_renewal_endpoint());
+    let http_token_retriever = match &auth_method {
+        AuthMethod::ClientSecret(client_secret) => TokenRetrieverWithCache::new_with_secret(
+            client_id.to_owned(),
+            http_authenticator,
+            client_secret.to_owned(),
+        ),
+        AuthMethod::PrivateKey(private_key_pem) => {
+            let jwt_signer =
+                JwtSignerImpl::Local(LocalPrivateKeySigner::try_from(private_key_pem)?);
+            TokenRetrieverWithCache::new_with_jwt_signer(
+                client_id.to_owned(),
+                http_authenticator,
+                jwt_signer,
+            )
+        }
+    };
+
+    let system_identity_creation_metadata = SystemIdentityCreationMetadata {
+        system_identity_input: SystemIdentityInput {
+            organization_id,
+            client_id,
+            auth_method,
+        },
+        name: format!("example-{}", env!("CARGO_BIN_NAME")).into(),
+        environment,
+        output_platform,
+    };
+
+    let iam_client = HttpIAMClient::new(
+        http_client,
+        http_token_retriever,
+        system_identity_creation_metadata.to_owned(),
+    );
+
+    let key_creator = LocalCreator::from(KeyPairGeneratorLocalConfig {
+        key_type: KeyType::Rsa4096,
+        name: "example-created-key".to_string(),
+        path: key_path, // Note how this is related to AuthOutputPlatform above!
+    });
+
+    let system_identity_generator = L2SystemIdentityGenerator {
+        iam_client,
+        key_creator,
+    };
+
+    let result = system_identity_generator.generate()?;
+
+    println!("System Identity created successfully: {result:?}");
+
+    Ok(())
+}
--- src/system_identity.rs
+++ src/system_identity.rs
@@ -1,5 +1,6 @@
 pub mod creation_response;
 pub mod generator;
+pub mod iam_client;
 pub mod identity_creator;
 pub mod input_data;
 
@@ -30,14 +31,182 @@ type Base64PublicKey = String; // For L2 System Identity.
 #[cfg(test)]
 mod tests {
     use mockall::Sequence;
+    use rstest::{rstest, Context};
+    use std::path::PathBuf;
 
     use crate::{
+        http_client::tests::MockHttpClient,
+        jwt::signer::local::{test::RS256_PRIVATE_KEY, LocalPrivateKeySigner},
         key::creator::tests::MockCreator,
         system_identity::{
-            generator::L2SystemIdentityGenerator, identity_creator::tests::MockL2IAMClient,
+            generator::L2SystemIdentityGenerator,
+            iam_client::http_impl::HttpIAMClient,
+            identity_creator::tests::MockL2IAMClient,
+            input_data::{
+                auth_method::ClientSecret, environment::NewRelicEnvironment,
+                output_platform::OutputPlatform, SystemIdentityCreationMetadata,
+                SystemIdentityInput,
+            },
             SystemIdentity,
         },
+        token_retriever::TokenRetrieverWithCache,
     };
+
+    use super::input_data::auth_method::AuthMethod;
+    // The idea here is emulate what would be the flow of a CLI call to create a system identity.
+    // The CLI would parse the command line arguments into a type with the actual concretions
+    // required to perform the operation. Here we test mocking only the HTTP client and the
+    // key creator. The rest are concretions that receive these as generics.
+    #[rstest]
+    #[case(AuthMethod::ClientSecret(ClientSecret::from("client-secret")))]
+    #[case(AuthMethod::PrivateKey(RS256_PRIVATE_KEY.as_bytes().into()))]
+    fn http_client_create_system_identity_from_client_secret(
+        #[context] ctx: Context,
+        #[case] auth_method: AuthMethod,
+    ) {
+        use crate::{authenticator::HttpAuthenticator, jwt::signer::JwtSignerImpl};
+
+        let cli_input = SystemIdentityCreationMetadata {
+            system_identity_input: SystemIdentityInput {
+                organization_id: "org-id".to_string(),
+                client_id: format!("{}-client-id", ctx.name),
+                auth_method,
+            },
+            name: ctx.name.to_string().into(),
+            environment: NewRelicEnvironment::Staging,
+            output_platform: OutputPlatform::LocalPrivateKeyPath(PathBuf::default()),
+        };
+        let expected_client_id = cli_input.system_identity_input.client_id.to_owned();
+        let expected_name = cli_input.name.to_owned();
+
+        let mut token_retriever_http_client = MockHttpClient::new();
+        // 1. Sign the token
+        token_retriever_http_client
+            .expect_send()
+            .once()
+            .withf({
+                let env = cli_input.environment.clone();
+                move |req| {
+                    req.method().eq(&http::Method::POST)
+                        && req.uri().eq(&env.token_renewal_endpoint())
+                        && req
+                            .headers()
+                            .get("Content-Type")
+                            .is_some_and(|v| v == "application/json")
+                }
+            })
+            .returning(|_| {
+                let response = http::Response::builder()
+                    .status(200)
+                    .body(
+                        serde_json::to_string(&serde_json::json!({
+                            "access_token": "some-access-token",
+                            "expires_in": 3600,
+                            "token_type": "Bearer",
+                        }))
+                        .unwrap()
+                        .as_bytes()
+                        .to_vec(),
+                    )
+                    .unwrap();
+                Ok(response)
+            });
+
+        let mut iam_client_http_client = MockHttpClient::new();
+        iam_client_http_client
+            .expect_send()
+            .once()
+            .withf({
+                let env = cli_input.environment.clone();
+                move |req| {
+                    req.method().eq(&http::Method::POST)
+                        && req.uri().eq(&env.identity_creation_endpoint())
+                        && req
+                            .headers()
+                            .get("Content-Type")
+                            .is_some_and(|v| v == "application/json")
+                }
+            })
+            .returning(move |_| {
+                let response = http::Response::builder()
+                    .status(200)
+                    .body(
+                        serde_json::to_string(&serde_json::json!({
+                          "data": {
+                            "systemIdentityCreate": {
+                              "clientId": format!("{}-client-id", ctx.name),
+                              "name": ctx.name,
+                              "publicKey": String::from_utf8_lossy(&[1u8, 2u8, 3u8]), // "cHVibGljS2V5QmFzZTY0RW5jb2RlZFN0cmluZw==",
+                              "id": "some-granted-id",
+                              "organizationId": "org-id",
+                              // "clientSecret": "c2VjcmV0LWNsaWVudC1zZWNyZXQtc2VjcmV0",
+                              // "credentialExpiration": "2023-10-01T00:00:00Z"
+                            }
+                          }
+                        }))
+                        .unwrap()
+                        .as_bytes()
+                        .to_vec(),
+                    )
+                    .unwrap();
+                Ok(response)
+            });
+
+        // More mocks, this time for the key creator
+        let mut key_creator = MockCreator::new();
+        key_creator
+            .expect_create()
+            .once()
+            .returning(|| Ok(vec![1, 2, 3]));
+
+        let authenticator = HttpAuthenticator::new(
+            token_retriever_http_client,
+            cli_input.environment.token_renewal_endpoint(),
+        );
+        let client_id = cli_input.system_identity_input.client_id.to_owned();
+        let token_retriever = match cli_input.system_identity_input.auth_method.to_owned() {
+            AuthMethod::ClientSecret(client_secret) => {
+                TokenRetrieverWithCache::new_with_secret(client_id, authenticator, client_secret)
+            }
+            AuthMethod::PrivateKey(private_key_pem) => {
+                let signer = LocalPrivateKeySigner::try_from(private_key_pem).unwrap();
+                let jwt_signer = JwtSignerImpl::Local(signer);
+                TokenRetrieverWithCache::new_with_jwt_signer(client_id, authenticator, jwt_signer)
+            }
+        };
+
+        // IAMClient from HttpClient
+        let iam_client = HttpIAMClient::new(
+            iam_client_http_client,
+            token_retriever,
+            cli_input, // I compare with this value later on, so we keep it here
+        );
+
+        // As we are creating concretions, we only need to set expectations for the key creator
+        // (which could be just abstracting the filesystem) and the HTTP client.
+        // However, the final structures that we create are actually generic over
+        // `IAMClient`s and `KeyCreator`s, so that makes it extensible for other, non-HTTP-based
+        // implementations.
+        let system_identity_generator = L2SystemIdentityGenerator {
+            key_creator,
+            iam_client,
+        };
+
+        let result = system_identity_generator.generate();
+        assert!(
+            result.is_ok(),
+            "Failed to generate system identity: {:?}",
+            result
+        );
+
+        let result = result.unwrap();
+        assert_eq!(result.name, expected_name);
+        assert_eq!(result.client_id, expected_client_id);
+        assert!(matches!(
+            result.identity_type,
+            super::SystemIdentityType::L2 { pub_key } if pub_key == String::from_utf8_lossy(&[1u8, 2u8, 3u8])
+        ));
+    }
 
     #[test]
     fn create_system_identity_mocked() {
