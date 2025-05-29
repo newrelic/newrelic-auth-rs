92b68bbe3b5dc074f95b51716a3ca0c914981392
David Sánchez <davidslt+git@pm.me>
2025-05-29 08:56:26 +0100

    style: move `PrivateKeyPem` to `key` module

--- src/system_identity/input_data/auth_method.rs
+++ src/system_identity/input_data/auth_method.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 use serde::{Deserialize, Serialize};
 
-use crate::jwt::signer::local::{LocalPrivateKeySigner, LocalPrivateKeySignerError};
+use crate::key::PrivateKeyPem;
 
 /// Represents the supported authentication methods with which a System Identity can be created.
 #[derive(Clone, PartialEq)]
@@ -26,28 +26,5 @@ pub struct ClientSecret(String); // TODO is String the correct inner representat
 impl<S: AsRef<str>> From<S> for ClientSecret {
     fn from(secret: S) -> Self {
         ClientSecret(secret.as_ref().to_string())
-    }
-}
-
-#[derive(Clone, PartialEq)]
-pub struct PrivateKeyPem(Vec<u8>);
-
-impl<S: AsRef<[u8]>> From<S> for PrivateKeyPem {
-    fn from(key: S) -> Self {
-        PrivateKeyPem(key.as_ref().to_vec())
-    }
-}
-
-impl TryFrom<PrivateKeyPem> for LocalPrivateKeySigner {
-    type Error = LocalPrivateKeySignerError;
-    fn try_from(value: PrivateKeyPem) -> Result<Self, Self::Error> {
-        Self::try_from(value.0.as_slice())
-    }
-}
-
-impl TryFrom<&PrivateKeyPem> for LocalPrivateKeySigner {
-    type Error = LocalPrivateKeySignerError;
-    fn try_from(value: &PrivateKeyPem) -> Result<Self, Self::Error> {
-        Self::try_from(value.0.as_slice())
     }
 }
