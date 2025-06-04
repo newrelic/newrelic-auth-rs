d75345a7d808a0253151737808949056e8c0f258
David Sánchez <davidslt+git@pm.me>
2025-06-04 16:55:32 +0100

    fix: debug impl str

--- src/system_identity/input_data/auth_method.rs
+++ src/system_identity/input_data/auth_method.rs
@@ -15,7 +15,7 @@ impl fmt::Debug for AuthMethod {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             AuthMethod::ClientSecret(_) => write!(f, "AuthMethod::ClientSecret(<REDACTED>)"),
-            AuthMethod::PrivateKey(_) => write!(f, "AuthMethod::PrivateKey(<REDACTED>"),
+            AuthMethod::PrivateKey(_) => write!(f, "AuthMethod::PrivateKey(<REDACTED>)"),
         }
     }
 }
